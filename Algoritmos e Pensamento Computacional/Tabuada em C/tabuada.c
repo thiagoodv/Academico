1fb4ae97454e68b8c69e911fca31d30198740f8d
Thiago Oliveira <thiagooliveira.dvale@gmail.com>
2026-09-11 11:29:05 -0300

    Add files via upload

---
+++ Algoritmos e Pensamento Computacional/Tabuada em C/tabuada.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int n, i = 1;
+   
+   printf("Digite um número para ver a tabuada: ");
+   scanf("%d", &n);
+   
+   do {
+       printf("%d x %d = %d\n", n, i, n * i);
+       i++;
+   } while (i <= 10);
+   
+   return 0;
+}
